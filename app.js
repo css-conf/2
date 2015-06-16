99cfdad7ac9c142a9be964af921bfcff680a1191
sofish <happinesz@gmail.com>
2015-06-16 16:34:25 +0800

    去掉自定义的 $

--- app.js
+++ app.js
@@ -1,15 +1,10 @@
-function $(sel) {
-  if(/^[#][^\s]+$/.test(sel)) return document.querySelector(sel);
-  return [].slice.call(document.querySelectorAll(sel));
-}
-
 fullpage.initialize('#wrapper', {
   menu: '#nav',
   sectionSelector: '.section',
   css3: true
 });
 
-$('[data-section]').forEach(function(sec) {
+[].slice.call(document.querySelectorAll('[data-section]')).forEach(function(sec) {
   sec.addEventListener('click', function(e) {
     e.preventDefault();
     var index = sec.getAttribute('data-section');
